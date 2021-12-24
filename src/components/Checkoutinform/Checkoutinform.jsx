dc1a6144306864ffa3c4a3aba8a3351a6c089405
phattar4korn <phattarakorn7787@gmail.com>
2021-12-24 15:57:01 +0700

    update button status

--- src/components/Checkoutinform/Checkoutinform.jsx
+++ src/components/Checkoutinform/Checkoutinform.jsx
@@ -1,8 +1,8 @@
-import styles from './Checkoutinform.module.css'
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-import SaveIcon from '@mui/icons-material/Save';
+import styles from './Checkoutinform.module.css';
 import { useEffect, useState } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
+import CheckIcon from '@mui/icons-material/Check';
+import ClearIcon from '@mui/icons-material/Clear';
 import {
     API_GET_Checkoutinform,
     API_CREATE_Checkoutinform,
@@ -12,6 +12,7 @@ import {
 
 import { API_UPDATE_Room ,API_GET_Rooms} from '../../API/Schema/Room/Room'
 import { API_queryRooms, API_queryBuildings, API_updateMeterRoomkwh, API_updateMeterRoomwater } from '../../API/index';
+import fetchData from '../../cores/axios';
 
 
 
@@ -26,7 +27,7 @@ const filter_rooms = (rooms , options_search) =>{
                         return (room.room && room.room.search(options_search.text) !== -1 ) ||
                         (room.building && room.building.search(options_search.text) !== -1 ) || 
                         (room.floor && room.floor.search(options_search.text) !== -1 ) ||
-                        (room.roomtype && room.roomtype.search(options_search.text) !== -1 ) ||
+                        (room.RoomType && room.RoomType.search(options_search.text) !== -1 ) ||
                         (room.status && room.status.search(options_search.text) !== -1 ) ||
                         (room.name && room.name.search(options_search.text) !== -1 ) ||
                         (room.surname && room.surname.search(options_search.text) !== -1 ) ||
@@ -43,7 +44,7 @@ const filter_rooms = (rooms , options_search) =>{
                         return (room.floor.search(options_search.text) !== -1 )	||
                         (options_search.text === '')	
                     }else if( options_search.keyword === 'ประเภทห้อง'){
-                        return (room.roomtype.search(options_search.text) !== -1 )	||
+                        return (room.RoomType && room.RoomType.search(options_search.text) !== -1 )	||
                         (options_search.text === '')		
                     }else{
                         return false; 
@@ -58,51 +59,49 @@ const filter_rooms = (rooms , options_search) =>{
 }
 
 
-const getRooms = async () => {
-	return new Promise(async (resolve, reject) => {
-		
-		let res = await API_queryRooms();
-        console.log("first-res",res)
-		let table = [];
-		if (res && res.status === 200) {
-			console.log(res.data.rooms);
-			table = res.data.rooms.map((data) => {
-				let _data = data;
-				return {
-					id: data.id,
-					data: _data,
-                    checkout:data.checkout_date,
-                    room : data.name ? data.name : '---',
-                    roomtype : data.RoomType ? data.RoomType.name : '---',
-					building:
-						data.floor && data.floor.building && data.floor.building.name
-							? data.floor.building.name
-							: '---',
-					floor: data.floor ? data.floor.name : '---',
-					name : ( data.members && data.members.length > 0 ) ? data.members[0].name : '---',
-                    surname : ( data.members && data.members.length > 0 ) ? data.members[0].lastname : '---',
-					status: data.status ? data.status : '---',
-                    
-				};
-			});
-		}
-
-		resolve(table);
-	}).catch((e) => {
-		console.log('Promise Error', e);
-		return [];
-	});
-};
-
-
-
-
-
 
 
 
 
 export const Checkoutinform = () => {
+
+
+    const getRooms = async () => {
+        return new Promise(async (resolve, reject) => {
+            
+            let res = await API_queryRooms();
+            console.log("get-first-res",res)
+            let table = [];
+            if (res && res.status === 200) {
+                table = res.data.rooms.map((data) => {
+                    let _data = data;
+                    return {
+                        id: data.id,
+                        data: _data,
+                        checkout:data.checkout_date,
+                        room : data.name ? data.name : '---',
+                        RoomType : data.RoomType ? data.RoomType.name : '---',
+                        building:
+                            data.floor && data.floor.building && data.floor.building.name
+                                ? data.floor.building.name
+                                : '---',
+                        floor: data.floor ? data.floor.name : '---',
+                        name : ( data.members && data.members.length > 0 ) ? data.members[0].name : '---',
+                        surname : ( data.members && data.members.length > 0 ) ? data.members[0].lastname : '---',
+                        status: data.status ? data.status : '---',
+                        
+                    };
+                });
+            }
+    
+            resolve(table);
+        }).catch((e) => {
+            console.log('Promise Error', e);
+            return [];
+        });
+    };
+
+
 
 
     
@@ -116,95 +115,40 @@ export const Checkoutinform = () => {
     const [ loadingpage, setloadingpage ] = useState(false);
 
     const Checkoutinform = useQuery(API_GET_Rooms);
-    console.log("first-data",Checkoutinform)
-    
 
-    const [checkoutinforms,setcheckoutinforms] = useState([])
+    const [checkoutinforms,setcheckoutinforms] = useState()
     const [updateStatus , mutationupdatestatus] = useMutation(API_UPDATE_Room)
-    const saveconfig = async () => {
-        let savebutton = document.querySelector('#save')
-
-        if(savebutton.click){
-            
-            try{
-
-                let _res = await updateStatus({
-
-                    variables: {
-                        id:`${checkboxs_select}`,
-                        input:{
-                            status: "ย้ายออก"
-                            }
-                        }
-
-
-                    });
-                    console.log(_res)
-
-
-            }catch(error){
-                console.log(error.message)
-            }
-
-    }
-    
-    }
-
-    const editconfig = () => {
-        let editbutton = document.querySelector('#edit')
-
-        if(editbutton.click){
-            
-            try{
-
-                let _res =  updateStatus({
-
-                    variables: {
-                        id:`${checkboxs_select}`,
-                        input:{
-                            status: "มีคนอยู่"
-                            }
-                        }
 
 
 
-
-
-
-
-
-                    });
-                    console.log(_res)
-
-
-
-            }catch(error){
-                console.log(error.message)
-            }
-
-    }
-    
-    }
-
-     
    
-    
 
 
+    useEffect(() => {
+        
+        if(Checkoutinform && Checkoutinform.data && Checkoutinform.data.Rooms){
+            console.log("checkinform-1",Checkoutinform.data.Rooms);
+            let _checkoutinforms = checkoutinforms
+            _checkoutinforms = Checkoutinform.data.Rooms
 
-    
+            setcheckoutinforms(_checkoutinforms)
+            console.log("checkinform-2",checkoutinforms);
+        }
+
+    })
+
+ 
     useEffect(
         () => {
             async function fetchData() {
                 let Rooms = await getRooms();
-                console.log('Rooms', Rooms);
                 setrooms(Rooms);
                 setfilterrooms(Rooms);
             }
             fetchData();
             setloadingpage(true);
         },
-        [ loadingpage ]
+        [ loadingpage,Checkoutinform ]
     );
 
 
@@ -284,27 +228,31 @@ export const Checkoutinform = () => {
                                                     <td width={'60px'}>{room.room ? room.room : '---'}</td>
                                                     <td width={'60px'} >{room.building ? room.building : '---'}</td>
                                                     <td width={'60px'} >{room.floor ? room.floor : '---'}</td>
-                                                    <td width={'80px'} >{room.roomtype ? room.roomtype : '---'}</td>
+                                                    <td width={'80px'} >{room.RoomType ? room.RoomType : '---'}</td>
                                                     <td width={'80px'} >{room.status ? room.status : '---'}</td>
                                                     <td width={'100px'} >{room.name}</td>
                                                     <td width={'100px'} >{room.surname}</td>
                                                     <td width={'80px'} >{room.checkout ? room.checkout : '---'}</td>
-                                                    <td width={'60px'} ><button 
-                                                    className={styles.buttonrow}
-                                                    type = 'checkbox'
-                                                    value={room.id}
-                                                    onClick={(e) =>{
+                                                    <td width={'60px'} >
+                                                        <button 
+                                                    className={styles.CheckButton}
+                                                    onClick={() =>{
+        
                                                         try{
                                                             let _res = updateStatus({
                                                             
                                                                 variables: {
-                                                                    id:`${e.target.value}`,
+                                                                    id:`${room.id}`,
                                                                     input:{
                                                                         status: "ย้ายออก"
                                                                         }
                                                                     }});
 
-                                                            filterrooms.refetch()
+                                                            if(_res){
+                                                                Checkoutinform.refetch()
+                                                            }
+//                                                            window.location.reload();
+                                    
                                                         }catch(error){
                                                             console.log(error)
                                                         }
@@ -312,7 +260,40 @@ export const Checkoutinform = () => {
                                                                 
                                                     }}
                                                     
-                                                    >ย้ายออก</button></td>
+                                                    ><CheckIcon/></button>
+                                                    <button
+                                                    className={styles.ClearButton}
+                                                    onClick={() =>{
+        
+                                                        try{
+                                                            let _res = updateStatus({
+                                                            
+                                                                variables: {
+                                                                    id:`${room.id}`,
+                                                                    input:{
+                                                                        status: "มีคนอยู่"
+                                                                        }
+                                                                    }});
+
+                                                            if(_res){
+                                                                Checkoutinform.refetch()
+                                                            }
+
+                                    
+                                                        }catch(error){
+                                                            console.log(error)
+                                                        }
+                                                                
+                                                                
+                                                    }}
+                                                    ><ClearIcon/></button>
+                                                    
+                                                    
+                                                    
+                                                    
+                                                    
+                                                    
+                                                    </td>
                                                 </tr>
                                             ) : null
                                     )}
@@ -320,15 +301,6 @@ export const Checkoutinform = () => {
 
                         </table>
                     </div>
-                    <div className= {styles.buttonstyles} >
-                        <button display = "disabled" id = 'save' className= {styles.buttonsave} onClick={ saveconfig
-                            
-                        }><SaveIcon/>บันทึก</button>
-                        <button id= 'edit' className= {styles.buttoncancel} onClick={ editconfig 
-                            
-                        }><HighlightOffIcon/>แก้ไข</button>
-                    </div>
-
                 </div>
             </div>
         
