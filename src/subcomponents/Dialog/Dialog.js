be19b99b43a0ac58c8656b06577648d380315d80
phattar4korn <phattarakorn7787@gmail.com>
2022-02-10 17:00:04 +0700

    Dialog next page

---
+++ src/subcomponents/Dialog/Dialog.js
@@ -0,0 +1,70 @@
+import styles from "./Dialog.module.css"
+import { useState } from "react";
+
+export default function Dialog({message , onDialog , nextPage}){
+
+    return(
+
+        <div className={styles.containerDialog}>           
+            <div className={styles.body}>
+                <h2>{message}</h2>
+            </div>
+            
+            <div className={styles.footer}>
+                <a 
+                href={nextPage}
+                ><button className={styles.confirm}
+                onClick={
+                    ()=>onDialog(true)
+                    }
+                    >Next page</button>
+                    </a>
+
+
+
+                <button className={styles.cancel} 
+                onClick={
+                    ()=>onDialog(false)
+                }
+                >close</button>
+            </div>
+
+        </div>
+
+    )
+}
+
+
+export const DialogFunction = () =>{
+
+    const [defaultDialog, setdefaultDialog] = useState({
+        message:"The request was successful. Next page?",
+        isLoading: false,
+        type:"",
+    });
+    const handleDialog = (message , isLoading ,type) =>{
+        setdefaultDialog({
+            message:message,
+            isLoading:isLoading,
+            type:type,
+        })
+    }
+
+    const checkData = async (e) =>{
+        if(e){
+            console.log('Next Page')
+            handleDialog(defaultDialog.message,false)
+
+        }else{
+            console.log('Stay Page')
+            handleDialog(defaultDialog.message,false)
+        }
+    }
+
+    return {defaultDialog ,setdefaultDialog , handleDialog , checkData}
+
+
+
+
+
+}
