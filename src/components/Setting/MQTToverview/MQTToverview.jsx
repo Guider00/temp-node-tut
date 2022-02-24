2f82b1b15d07560769b664840ea3a2b7514c3250
phattar4korn <phattarakorn7787@gmail.com>
2022-02-24 09:28:49 +0700

    Update CSS MQTT

--- src/components/Setting/MQTToverview/MQTToverview.jsx
+++ src/components/Setting/MQTToverview/MQTToverview.jsx
@@ -1,104 +1,185 @@
-import { useEffect,  useState } from "react"
-
+import { useEffect, useState } from "react"
+import styles from './MQTToverview.module.css'
 import { useSubscription } from '@apollo/client';
-import {  Sub_MQTTHISTORY , Sub_MQTTClients } from '../../../API/Schema/setting/MQTToverview/MQTToverview'
-export const MQTToverview = () =>{
-    const [showhistory,setshowhistory] = useState(false) 
-    const [showclient,setshowclient] = useState(false)
-    const [historys,sethistorys] = useState([])
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import CancelIcon from '@mui/icons-material/Cancel';
+import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
+import { Sub_MQTTHISTORY, Sub_MQTTClients } from '../../../API/Schema/setting/MQTToverview/MQTToverview'
+export const MQTToverview = () => {
+   const [showhistory, setshowhistory] = useState(false)
+   const [showclient, setshowclient] = useState(false)
+   const [historys, sethistorys] = useState([])
 
 
 
-    const handlerstartmqttserver =() =>{
 
-    }
-    const handlerstopmqttserver =()=>{
 
-    }
-    const hnalderreconnectmqttserver =()=>{
 
-    }
-    const sub_mqtthistory = useSubscription(Sub_MQTTHISTORY );
-    const sub_mqttclient  = useSubscription(Sub_MQTTClients)
-    
-    console.log(sub_mqttclient)
+   //ฟังก์ชั่นโชว์ข้อมูล
+   const [clickedHistory, setClickedHistory] = useState(false);
 
-    
-    return (
-        <> 
-          <h1> MQTToverview </h1>
-          <div>
-             <h2> MQTT Sever Status </h2>
-             <div>
-                <button>start</button>
-                <button>stop</button>
-                <button>reconnect </button>
-             </div>
-          </div>
-          <div>
-             <h2> Clients
-              <button style={{ backgroundColor:(showclient)?"green":"red" }}onClick={()=>{setshowclient(!showclient)}}> arrowdown </button>
-             </h2>
-             <div>
-                {  
-                  (showclient && sub_mqttclient &&  sub_mqttclient.data  && sub_mqttclient.data.submqttabaseclients ) ?
-                    <ul>
-                         {
-                            sub_mqttclient.data.submqttabaseclients.map( (client,index)=>(
-                           <li   style={{border: "1px solid black",listStylePosition:'inside',padding:"1rem"}} key={index+1} name={`li-history${index}`}>
-                                 {client.id}
-                              
-                                 
-                           </li>
-                           ) )
+   const toggle = (index) => {
+      if (clickedHistory === index) {
+         return setClickedHistory(null)
+      }
+
+      setClickedHistory(index)
+
+   }
+  
+
+
+   const handlerstartmqttserver = () => {
+
+   }
+   const handlerstopmqttserver = () => {
+
+   }
+   const hnalderreconnectmqttserver = () => {
+
+   }
+   const sub_mqtthistory = useSubscription(Sub_MQTTHISTORY);
+   const sub_mqttclient = useSubscription(Sub_MQTTClients)
+
+   console.log(sub_mqttclient)
+
+
+   return (
+      <>
+         <div className={styles.container}>
+            <h1 className={styles.Content} > MQTToverview </h1>
+            <div className={styles.card}>
+               <div className={styles.header}>
+                  <div className={styles.Status} >
+                     <h2> MQTT Sever Status </h2>
+                     <div>
+                        <div>
+                           <button style={{ backgroundColor: '#79FC70' }}><ThumbUpAltIcon /></button>
+                           <div>Good</div>
+                        </div>
+                        <div>
+                           <button style={{ backgroundColor: '#FC7070' }} ><CancelIcon /></button>
+                           <div>Stop</div>
+                        </div>
+                        <div>
+                           <button style={{ backgroundColor: '#FFCC7F' }} ><HourglassBottomIcon /></button>
+                           <div>Reconnect</div>
+                        </div>
+
+
+                     </div>
+
+                  </div>
+                  <div className={styles.lastMessage}>
+                     <div className={styles.head} >
+                        <h2>last message</h2>
+                     </div>
+                     <div className={styles.messageData}>
+                        {
+                           sub_mqtthistory.data && sub_mqtthistory.data.mqtthistory_packets && sub_mqtthistory.data.mqtthistory_packets.length > 0 ?
+                              <div>
+                                 <h5> topic  {sub_mqtthistory.data.mqtthistory_packets[0].topic} </h5>
+                                 <div>
+                                    <h5>payload</h5>
+                                    {sub_mqtthistory.data.mqtthistory_packets[0].payload}
+                                 </div>
+                              </div>
+                              : null
                         }
-                     </ul>
-                    :null
-                }
-             </div>
-          </div>
-          <div>
-             <h2>last message</h2>
-            <div>
-              {  
-              sub_mqtthistory.data && sub_mqtthistory.data.mqtthistory_packets && sub_mqtthistory.data.mqtthistory_packets.length > 0 ?
-              <div>
-                <h5> topic  {sub_mqtthistory.data.mqtthistory_packets[0].topic} </h5>
-                 <div>
-                     <h5>payload</h5>
-                     {sub_mqtthistory.data.mqtthistory_packets[0].payload}
-                 </div>
-              </div>
-              :null
-              }
+                     </div>
+
+                  </div>
+
+               </div>
+               <div className={styles.body}>
+                  <div className={styles.clients}>
+                     <div className={styles.head}>
+                        <h2>Clients</h2>
+                        <button style={{ backgroundColor: (showclient) ? "green" : "red" }} onClick={() => { setshowclient(!showclient) }}>
+                           <label>information</label>
+                           <ExpandMoreIcon className={(showclient) ? styles.upIcon : styles.downIcon} />
+                        </button>
+                     </div>
+
+                     <div className={styles.clientData}>
+                        {
+                           (showclient && sub_mqttclient && sub_mqttclient.data && sub_mqttclient.data.submqttabaseclients) ?
+                              <ul>
+                                 {
+                                    sub_mqttclient.data.submqttabaseclients.map((client, index) => (
+                                       <li style={{ border: "1px solid black", listStylePosition: 'inside', padding: "1rem" }} key={index + 1} name={`li-history${index}`}>
+                                          {client.id}
+
+
+                                       </li>
+                                    ))
+                                 }
+                              </ul>
+                              : null
+                        }
+
+                     </div>
+
+                  </div>
+                  <div className={styles.viewHistory}>
+                     <div className={styles.head}>
+                        <h2>View History</h2>
+                        <button style={{ backgroundColor: (showhistory) ? "green" : "red" }} onClick={() => { setshowhistory(!showhistory) }}>
+                           <label>information</label>
+                           <ExpandMoreIcon className={(showhistory) ? styles.upIcon : styles.downIcon} />
+                        </button>
+                     </div>
+                     <div
+                        className={styles.historyTopic}>
+                        {(showhistory && sub_mqtthistory.data && sub_mqtthistory.data.mqtthistory_packets) ?
+
+                           <ul>
+                              {
+                                 sub_mqtthistory.data.mqtthistory_packets.map((history, index) => (
+                                    <li
+                                       style={{ border: "1px solid black", listStylePosition: 'inside', padding: "1rem" }}
+                                       key={index + 1}
+                                       name={`li-history${index}`}
+                                       onClick={() => toggle(index)}
+                                    >
+                                       {index + 1}  topic  {history.topic}
+
+                                       <div className={styles.historyInfo}>
+                                          {clickedHistory === index ?
+                                             <div>
+                                                <h5>payload</h5>
+                                                {history.payload}
+
+                                             </div>
+                                             : null}
+
+                                       </div>
+                                    </li>
+                                 ))
+                              }
+                           </ul>
+                           : null
+                        }
+
+                     </div>
+
+                  </div>
+
+               </div>
+               <div className={styles.body}>
+
+               </div>
+
             </div>
 
-          </div>
-          <div>
-             <h2> 
-                View History  
-                <button style={{ backgroundColor:(showhistory)?"green":"red" }}onClick={()=>{setshowhistory(!showhistory)}}> arrowdown </button>
-             </h2>
-             {(showhistory && sub_mqtthistory.data && sub_mqtthistory.data.mqtthistory_packets)?
-               
-                <ul>
-                    {
-                    sub_mqtthistory.data.mqtthistory_packets.map( (history,index)=>(
-                        <li   style={{border: "1px solid black",listStylePosition:'inside',padding:"1rem"}} key={index+1} name={`li-history${index}`}>
-                                {index+1}  topic  {history.topic}
-                             
-                              <div >
-                                 <h5>payload</h5>
-                                 {history.payload}
-                              </div>
-                        </li>
-                    ) )
-                    }
-                </ul>
-                :null
-             }
-             
-          </div>
-        </>
-    )
+         </div>
+
+
+
+
+      </>
+   )
 }
+
+
