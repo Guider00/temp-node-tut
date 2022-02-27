2b74c279b50952e0545745edc0ac9b65e98c5081
phattar4korn <phattarakorn7787@gmail.com>
2022-02-27 23:56:38 +0700

    Realtime Css

--- src/components/Setting/Realtimetable/Realtimetable.jsx
+++ src/components/Setting/Realtimetable/Realtimetable.jsx
@@ -9,15 +9,15 @@ const DEFAULTSTATTABLEINDEX = 1;
 const DEFAULTSIZETABLEINDEX = 10;
 const DEFAULTSTOPTABLEINDEX = DEFAULTSTATTABLEINDEX - 1 + DEFAULTSIZETABLEINDEX;
 export const Realtimetable = () => {
-	const [ devicename, setdevicename ] = useState('');
-	const [ ports, setports ] = useState([]);
-	const [ devices, setdevices ] = useState([]);
-	const [ portselected, setportselected ] = useState('---');
-	const [ deviceselected, setdeviceselected ] = useState('---');
+	const [devicename, setdevicename] = useState('');
+	const [ports, setports] = useState([]);
+	const [devices, setdevices] = useState([]);
+	const [portselected, setportselected] = useState('---');
+	const [deviceselected, setdeviceselected] = useState('---');
 
-	const [ tablestartindex, settablestartindex ] = useState(DEFAULTSTATTABLEINDEX);
-	const [ tablestoptindex, settablestoptindex ] = useState(DEFAULTSTOPTABLEINDEX);
-	const [ tableviewsize, settableviewsize ] = useState(DEFAULTSIZETABLEINDEX);
+	const [tablestartindex, settablestartindex] = useState(DEFAULTSTATTABLEINDEX);
+	const [tablestoptindex, settablestoptindex] = useState(DEFAULTSTOPTABLEINDEX);
+	const [tableviewsize, settableviewsize] = useState(DEFAULTSIZETABLEINDEX);
 	const Portmeters = useQuery(API_GET_Portmeters);
 	const MeterRooms = useQuery(API_GET_MeterRooms);
 
@@ -55,107 +55,146 @@ export const Realtimetable = () => {
 	useEffect(() => {
 		if (Portmeters.data) {
 			setports(JSON.parse(JSON.stringify(Portmeters.data.Portmeters)));
+			console.log(Portmeters.data)
 		}
-    if (MeterRooms.data) {
+		if (MeterRooms.data) {
 			setdevices(JSON.parse(JSON.stringify(MeterRooms.data.MeterRooms)));
+			console.log(MeterRooms.data)
 		}
-	}, [Portmeters.data,MeterRooms.data] );
+	}, [Portmeters.data, MeterRooms.data]);
 
 	return (
-		<div>
-			<h1> Realtimetable</h1>
-			<div className={styles.boxinput}>
-				<div className={styles.forminput}>
-					<div className={styles.row50}>
-						<label> Port </label>
-						<select name="port" onChange={handleronchange}>
-							{ports && ports.length > 0 ? ports.map((port) => <option>{port.name}</option>) : null}
-						</select>
-					</div>
-					<div className={styles.row50}>
-						<label> Device </label>
-						<select name="device" onChange={handleronchange}>
-							{devices && devices.length > 0 ? (
-								devices.map((device) => <option>{device.name}</option>)
-							) : null}
-						</select>
+		<div className={styles.container}>
+			<div className={styles.header}>
+				<h1> Realtimetable</h1>
+
+				<div className={styles.boxinput}>
+					<div className={styles.forminput}>
+						<div className={styles.row50}>
+							<label style={{ color: 'rgb(11, 101, 180)' }}> Port </label>
+							<select
+								style={{ cursor: 'pointer' }}
+								name="port" onChange={handleronchange}>
+								{ports && ports.length > 0 ? ports.map((port) => <option>{port.name}</option>) : null}
+							</select>
+						</div>
+						<div className={styles.row50}>
+							<label style={{ color: 'rgb(11, 101, 180)' }}> Device </label>
+							<select
+								style={{ cursor: 'pointer' }}
+								name="device" onChange={handleronchange}>
+								{devices && devices.length > 0 ? (
+									devices.map((device) => <option>{device.name}</option>)
+								) : null}
+							</select>
+						</div>
 					</div>
 				</div>
+
 			</div>
-			{subdevicemeterrealtime && subdevicemeterrealtime.data ? (
-				<div className={styles.boxtable}>
-					<div className={styles.row}>
-						<h3>
-							{' '}
-							Port: {portselected} Device:{deviceselected}{' '}
-						</h3>
-					</div>
-					<div className={styles.row}>
-						<table>
-							<thead>
-								<tr>
-									<th>#</th>
-									<th>Tag</th>
-									<th>Value</th>
-								</tr>
-							</thead>
-							<tbody>
-								{filter_datatable(
+			<div className={styles.body}>
+				{subdevicemeterrealtime && subdevicemeterrealtime.data ? (
+					<div className={styles.boxtable}>
+						<div className={styles.topic}>
+							<h3>
+								{' '}
+								Port : <label style={{ color: 'red', margin: '0.5rem' }}>{portselected}</label> Device :<label style={{ color: 'red', margin: '0.5rem' }} >{deviceselected}{' '}</label>
+							</h3>
+						</div>
+						<div className={styles.row}>
+							<table>
+								<thead>
+									<tr>
+										<th>#</th>
+										<th>Tag</th>
+										<th>Value</th>
+									</tr>
+								</thead>
+								<tbody>
+									{/* <tr className={styles.od}>
+										<td>1</td>
+										<td>2</td>
+										<td>3</td>
+									</tr>
+									<tr>
+										<td>1</td>
+										<td>2</td>
+										<td>3</td>
+									</tr>
+									<tr>
+										<td>1</td>
+										<td>2</td>
+										<td>3</td>
+									</tr>
+									<tr>
+										<td>1</td>
+										<td>2</td>
+										<td>3</td>
+									</tr> */}
+									{filter_datatable(
 									subdevicemeterrealtime.data.subdevicemeterrealtime,
 									portselected,
 									deviceselected
 								).map((list, index) => {
 									return index >= tablestartindex - 1 && index <= tablestoptindex - 1 ? (
-										<tr key={`tablelist${index}`}>
+										<tr key={`tablelist${index}`} className={styles.od}>
 											<td>{index + 1}</td>
 											<td>{list.tag}</td>
 											<td>{list.value}</td>
 										</tr>
 									) : null;
 								})}
-							</tbody>
-							<tfoot>
-								<tr>
-									<td colspan="3">
-										<button
-											name="back"
-											disabled={tablestartindex - tableviewsize <= 0}
-											onClick={handlerchagetableview}
-										>
-											{' '}
-											{`<`}
-										</button>
-										{tablestartindex} -- {tablestoptindex}
-										/length
-										{
-											filter_datatable(
-												subdevicemeterrealtime.data.subdevicemeterrealtime,
-												portselected,
-												deviceselected
-											).length
-										}
-										<button
-											name="next"
-											disabled={
-												tablestartindex + tableviewsize >
-												filter_datatable(
-													subdevicemeterrealtime.data.subdevicemeterrealtime,
-													portselected,
-													deviceselected
-												).length
-											}
-											onClick={handlerchagetableview}
-										>
-											{' '}
-											{`>`}{' '}
-										</button>
-									</td>
-								</tr>
-							</tfoot>
-						</table>
+								</tbody>
+								<tfoot>
+									<tr>
+										<td colspan="3">
+											<button
+												className={styles.button}
+												name="back"
+												disabled={tablestartindex - tableviewsize <= 0}
+												onClick={handlerchagetableview}
+											>
+												{' '}
+												{`<`}
+											</button>
+											<label className={styles.nextpage} style={{color:'rgb(41, 107, 206)'}}>{tablestartindex}</label> -- <label className={styles.nextpage} style={{color:'rgb(41, 107, 206)'}}>{tablestoptindex}</label>
+											/length
+											<label className={styles.nextpage} style={{color:'rgb(41, 107, 206)'}} >
+												{
+													filter_datatable(
+														subdevicemeterrealtime.data.subdevicemeterrealtime,
+														portselected,
+														deviceselected
+													).length
+												}
+
+											</label>
+											<button
+												className={styles.button}
+												name="next"
+												disabled={
+													tablestartindex + tableviewsize >
+													filter_datatable(
+														subdevicemeterrealtime.data.subdevicemeterrealtime,
+														portselected,
+														deviceselected
+													).length
+												}
+												onClick={handlerchagetableview}
+											>
+												{' '}
+												{`>`}{' '}
+											</button>
+										</td>
+									</tr>
+								</tfoot>
+							</table>
+						</div>
 					</div>
-				</div>
-			) : null}
+				) : null}
+
+			</div>
+
 		</div>
 	);
 };
