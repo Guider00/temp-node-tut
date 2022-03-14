92a126024041b59841950543d1ea6a7205aadd69
zungzeny808 <zungzeny808@gmail.com>
2022-03-14 11:31:32 +0700

    update test-webapplication-file

---
+++ test-webreact.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const log4js = require("log4js");
+log4js.configure({
+  appenders: { cheese: { type: "file", filename: "cheese.log" } },
+  categories: { default: { appenders: ["cheese"], level: "trace" } }
+});
+
+const logger = log4js.getLogger("cheese");
+logger.trace("Entering cheese testing");
+logger.debug("Got cheese.");
+logger.info("Cheese is Comté.");
+logger.warn("Cheese is quite smelly.");
+logger.error("Cheese is too ripe!");
+logger.fatal("Cheese was breeding ground for listeria.");
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+app.listen(9000);
