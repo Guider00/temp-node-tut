006208dd46fb285eadd8791fe69f91678e916641
Patsawut <ph.patsawut_st@tni.ac.th>
2022-05-26 16:59:44 +0700

    first commit

---
+++ 8-os-module.js
@@ -0,0 +1,20 @@
+const os = require('os')
+const { release } = require('process')
+
+// info about curren user
+const user = os.userInfo()
+console.log(user)
+
+//method returns the system uptime in seconds
+
+
+console.log(`The system uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+}
+
+console.log(currentOS)
